dc761b5b91f11d2443975b1a03d37ab9015d3b8d
Pedro Martínez Barrientos <pedromba@mx1.ibm.com>
2021-09-07 23:49:11 -0500

    cambio de interfase para usar con Px-Enterprise

--- main.rb
+++ main.rb
@@ -10,47 +10,7 @@ set(:cookie_options) do
   { :expires => Time.now + 30*60 }
 end
 
-#************
-#Copiar y actualizar en cada módulo
-# ***Adaptar para que no se reescriban las rutas del módulo en particular donde se despliegue
-#************
-get '/cp4d' do
-  redirect "https://ui.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/cp4d?"
-  #redirect "http://localhost:8090"
-end
-get '/uidl' do
-  redirect "https://ui-ga.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/uidl"
-end
-get '/uiga' do
-  redirect "https://ui-ga.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/uiga"
-  #redirect "http://localhost:8090"
-end
-get '/loganalysis' do
-  redirect "https://ui-monitoring.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/VATLA?"
-  #redirect "http://localhost:8090"
-end
 
-get '/monitoring' do
-  redirect "https://ui-monitoring.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/VLG?"
-  #redirect "http://localhost:8090"
-end
-
-#get '/pxbackup' do
-#  redirect "https://pxbackup.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/"
-  #redirect "http://localhost:8090"
-#end
-get '/iks' do
-  redirect "https://iks-ocp.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/iks"
-end
-get '/ocp' do
-  redirect "https://iks-ocp.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/ocp"
-end
-get '/cr' do
-  redirect "https://ui-cr.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/"
-end
-#************
-#Fin Copiar y actualizar en cada módulo
-#************
 
 get '/' do
   logger = Logger.new(STDOUT)
@@ -61,11 +21,6 @@ get '/' do
   respuestastorage=[]
   response.set_cookie("llave2", value: "valor2")
     erb :pxbackup , :locals => {:respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
-end
-
-get '/regresar' do
-  redirect "https://ui.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/"
-  #redirect "http://localhost:4567"
 end
 
 get '/respuesta' do
@@ -111,4 +66,63 @@ get '/respuesta' do
   respuestasizing=JSON.parse(respuestasizing.to_s)
   logger.info(respuestasizing)
   erb :pxbackup , :locals => {:respuestasizing => respuestasizing}
+end
+
+
+
+get '/pxbackupyent' do
+  logger = Logger.new(STDOUT)
+  logger.info("Selecciono dimensionamiento para solución de respaldos")
+  @name = "PX-Backup"
+  respuestasizing=[]
+  respuestasizingalt=[]
+  respuestastorage=[]
+  response.set_cookie("llave2", value: "valor2")
+    erb :pxbackupyent , :locals => {:respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
+end
+
+get '/pxbackupyentrespuesta' do
+
+  logger = Logger.new(STDOUT)
+  logger.info("Recibiendo parametros para dimensionamiento de PX-backup:" +
+    "rsemanal:"+"#{params['rsemanal']}"+
+    "rsemanalretencion:"+ "#{params['rsemanalretencion']}"+
+    "rdiario: "+"#{params['rdiario']}"+
+    "rdiarioretencion:"+"#{params['rdiarioretencion']}"+
+    "rmensual: "+"#{params['rmensual']}"+
+    "rmensualretencion:"+"#{params['rmensualretencion']}"+
+    "ranual: "+"#{params['ranual']}"+
+    "ranualretencion:"+"#{params['ranualretencion']}"+
+    "regioncluster: "+"#{params['regioncluster']}"+
+    "almacenamientogb:"+"#{params['almacenamientogb']}"+
+    "countryrespaldo: "+"#{params['countryrespaldo']}"+
+    "resiliencybackup:"+"#{params['resiliencybackup']}")
+
+
+  almacenamientogb="#{params['almacenamientogb']}" #cantidad en GB
+  #parametros de politicas
+  rsemanal="#{params['rsemanal']}"
+  rsemanalretencion="#{params['rsemanalretencion']}" #cantidad de backups retenidos
+  rdiario="#{params['rdiario']}"
+  rdiarioretencion="#{params['rdiarioretencion']}"#cantidad de backups retenidos
+  rmensual="#{params['rmensual']}"
+  rmensualretencion="#{params['rmensualretencion']}"#cantidad de backups retenidos
+  ranual="#{params['ranual']}"
+  ranualretencion="#{params['ranualretencion']}"#cantidad de backups retenidos
+  regioncluster="#{params['regioncluster']}"#region del cluster de IKS donde se desplegará PX-Backup
+  countryrespaldo = "#{params['countryrespaldo']}"
+  resiliencybackup ="#{params['resiliencybackup']}"
+  diff="#{params['diff']}"
+  @name = "PX-Backup"
+  urlapi="https://apis.9sxuen7c9q9.us-south.codeengine.appdomain.cloud"
+  #urlapi="http://localhost:8080"
+
+  #parametros recibidos
+  callapi="#{urlapi}/api/lvl2/pxbackupsol_pxent?almacenamientogb=#{almacenamientogb}&rsemanal=#{rsemanal}&rsemanalretencion=#{rsemanalretencion}&rdiario=#{rdiario}&rdiarioretencion=#{rdiarioretencion}&rmensual=#{rmensual}&rmensualretencion=#{rmensualretencion}&ranual=#{ranual}&ranualretencion=#{ranualretencion}&regioncluster=#{regioncluster}&countryrespaldo=#{countryrespaldo}&resiliencybackup=#{resiliencybackup}&diff=#{diff}"
+  logger.info("llamado api:" )
+  logger.info(callapi)
+  respuestasizing = RestClient.get callapi, {:params => {}}
+  respuestasizing=JSON.parse(respuestasizing.to_s)
+  logger.info(respuestasizing)
+  erb :pxbackupyent , :locals => {:respuestasizing => respuestasizing}
 end
